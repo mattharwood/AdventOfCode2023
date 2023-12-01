6997c8b9162efec26feb34d02867c85d5b1fb69b
Matthew Harwood <matthew.harwood@bybox.com>
2023-12-01 11:41:05 +0000

    Day 1

---
+++ Aoc.Day1/Program.cs
@@ -0,0 +1,11 @@
+﻿var tokens = File.ReadAllLines("input.txt");
+var numbers = new List<int>();
+
+foreach (var token in tokens)
+{
+    var characters = token.ToCharArray();
+    var localNumbers = characters.Where(char.IsDigit).ToList();
+    numbers.Add(int.Parse($"{localNumbers.First()}{localNumbers.Last()}"));
+}
+
+Console.WriteLine(numbers.Sum());
